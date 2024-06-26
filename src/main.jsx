194080eac4e5198578f81a827375d7736eec44e9
Damon Windsor <damonwindsor@gmail.com>
2024-06-26 13:10:58 +1000

    Revert "."
    This reverts commit 0aaf18fbe8bbde04d496c573eee19a7b979b2d6c.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import '../particles.js';
-import '../app.js'; 
+import '../particles.js'; // Assuming particles.js has global side effects you need
+import '../app.js'; // Assuming app.js has global side effects you need
 
-import App from './App.jsx';
-import './index.css';
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
